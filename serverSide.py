c3410aba1a2e1876d5966775c14cd4c74fab7796
Mohamed25885 <memo_868@yahoo.com>
2021-11-08 13:23:17 +0200

    working on serverside

--- serverSide.py
+++ serverSide.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, Request, Response
+import DB.dbAccessLayer as dbAccessLayer
 
 app = FastAPI()
 
 
-@app.post("/")
-async def root(req:Request, res:Response):
-    pass
+@app.get("/{level}")
+async def root(level:int,req:Request, res:Response):
+    if level is not int:
+        res.status_code = 404
+        res.body = "level must be an integer value"
+        return res
+    else:
+        return dbAccessLayer.getLevel(level)
